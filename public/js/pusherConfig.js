38de6e0f3c6a57f7d02a01e46af68d69b17d519c
Abdullrahman22 <abdullrahman.ismael22@gmail.com>
2020-11-10 16:35:06 +0200

    Set Pusher Config

--- public/js/pusherConfig.js
+++ public/js/pusherConfig.js
@@ -1,1 +1,16 @@
 
+
+
+
+// Enable pusher logging - don't include this in production
+Pusher.logToConsole = true;
+
+var pusher = new Pusher('3f9a0b89de189c92500c', {
+     cluster: 'mt1'
+});
+
+var channel = pusher.subscribe('new-notification');
+// channel.bind('App\\Events\\NewNotification', function(data) {
+channel.bind('NewNotification', function(data) {
+     console.log(data);
+});
